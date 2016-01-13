41704f1b56c2356269db573a7c88e57fce8ad3d3
Alicia Poindexter <alcampbel@gmail.com>
2016-01-13 10:36:52 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BUDGET TRACKER
+
+## By Alicia Poindexter
---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict'
+
+$(document).ready(init);
+
+function init() {
+  var currentDate = moment().format('YYYY-MM-DD');
+  $('#transDate').attr('min', currentDate);
+
+  $('#newTrans').submit(addTrans);
+  $('table').on('click', '.delete button', deleteTrans);
+}
+
+function addTrans(e) {
+  e.preventDefault();
+
+  var transName = $('#transName').val();
+  var transDate = $('#transDate').val();
+  var formattedDate = moment(transDate).format('ll');
+  var deposit = ($('#transAmount').val() > 0) ? $('#transAmount').val() : null;
+  var withdrawal = ($('#transAmount').val() < 0) ? $('#transAmount').val() : null;
+
+  var $tr = $('#sample').clone();
+  $tr.removeAttr('id');
+  $tr.children('.trans').text(transName);
+  $tr.children('.date').text(formattedDate);
+  $tr.children('.credit').text(deposit);
+  $tr.children('.debit').text(withdrawal);
+  $('#transList').append($tr);
+}
+
+function deleteTrans() {
+  $(this).closest('tr').remove();
+}
